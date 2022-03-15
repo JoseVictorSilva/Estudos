0105aa3238f03e834ce0214e272d72c358bf5852
José Victor Nogueira Alves da Silva <josevictorcomrc@gmail.com>
2022-03-15 15:26:07 -0300

    Commit

--- Curso/LerArquivos/leitura.py
+++ Curso/LerArquivos/leitura.py
@@ -7,8 +7,12 @@ open() -> Na forma mais simples de utilização nós passamos apenas um parâmet
 que vai retornar um _io.TextIOWrapper, que é o que trabalhamos
 
 Erro: FileNotFoundError
+
+mode: 'r' -> reading
 """
 
-arquivo = open('texto.txt', 'r')
+arquivo = open('teste.txt')
 print(arquivo)
 print(type(arquivo))
+# Para ler o arquivo
+print(arquivo.read())
