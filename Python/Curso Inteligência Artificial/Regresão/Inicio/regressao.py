c0f4a5589bfd1940677ebedae36cbdf4330a75e5
= <josevictorcomrc@gmail.com>
2022-06-26 20:19:48 -0300

    Retirando erros

--- Python/Curso Inteligência Artificial/Regresão/Inicio/regressao.py
+++
@@ -1,8 +0,0 @@
-import plotly.express as px
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
-import numbers as np
-
-base_plano_saude = pd.read_csv('../ArquivosBase/plano_saude.csv')
-print(base_plano_saude)
