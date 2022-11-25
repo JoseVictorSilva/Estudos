955420f4c8f3a61e642d12035c8aaa64ac01db74
= <josevictorcomrc@gmail.com>
2022-11-25 02:04:42 -0300

    Atualizações 3

---
+++ Python/CursoVisaoComputacional/ReconhecimentoObjetos/CSRT.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+
+rastreador = cv.TrackerCSRT_create()
+
+video = cv.VideoCapture('../ArquivosBase/race.mp4')
+ok,frame = video.read()
+bbox = cv.selectROI(frame) #ROI Region Of Interest
+
+ok = rastreador.init(frame, bbox)
+
+while True:
+    ok ,frame = video.read()
+    if not ok: 
+        break
+    ok ,bbox = rastreador.update(frame)
+    print(ok)
+
+    if ok:
+        (x, y, w, h) = [int(v) for v in bbox]
+        cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2, 1)
+    else:
+        cv.putText(frame, 'Erro', (100,80), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
+    cv.imshow('Rastreamento',frame)
+    if cv.waitKey(1) & 0XFF == 27:
+        break
